7706bbda490ef0216d4f1a83054b1b6b5c82c46e
bonecrushereb <bonecrushereb@gmail.com>
2017-06-23 10:35:50 -0700

    add bodyparser, trying to connect to mongoose

--- backend/routes/youtuberouter.js
+++ backend/routes/youtuberouter.js
@@ -1,17 +1,22 @@
 const Router = require('express').Router;
+const youtube = require(__dirname + '/../models/youtube.js')
 const superAgent = require('superagent');
+const bodyParser = require('body-parser').json();
 var youtubeRouter = module.exports = exports = Router();
 
 youtubeRouter.post('/api/categories', (req, res) => {
 
-  var searchAddress = 'https://www.googleapis.com/youtube/v3/search';
+  console.log("hello from post");
 
-  superAgent
-    .get(searchAddress)
-    .send({ q: '' + keyword })
-    .set('Accept', 'application/json')
-    .end((err, data) => {
-      if (err) return console.log(err);
-      res.status(200).json(data);
-    });
+  // var searchAddress = 'https://www.googleapis.com/youtube/v3/search';
+  //
+  // superAgent
+  //   .get(searchAddress)
+  //   .send({ q: '' + keyword })
+  //   .set('key', process.env.YOUTUBE_API_KEY)
+  //   .set('Accept', 'application/json')
+  //   .end((err, data) => {
+  //     if (err) return console.log(err);
+  //     res.status(200).json(data);
+  //   });
 });
