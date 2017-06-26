fd44eed488d0bba82b4310e1ab8edb1e2c4691eb
bonecrushereb <bonecrushereb@gmail.com>
2017-06-26 11:36:53 -0700

    able to recieve data from youtube

--- backend/routes/youtuberouter.js
+++ backend/routes/youtuberouter.js
@@ -1,5 +1,5 @@
 const Router = require('express').Router;
-const Youtube = require(__dirname + '/../models/youtube.js')
+const Youtube = require(__dirname + '/../models/youtube.js');
 const superAgent = require('superagent');
 const bodyParser = require('body-parser').json();
 var youtubeRouter = module.exports = exports = Router();
@@ -9,5 +9,25 @@ youtubeRouter.post('/categories', bodyParser, (req, res) => {
   newYoutubeSearch.save((err, data) => {
     if (err) return console.log(err);
     res.status(200).json(data);
+  });
+});
+
+youtubeRouter.get('/categories', (req, res) => {
+  Youtube.findOne( null, (err, data) => {
+    if (err) console.log(err);
+    // res.status(200).json(data);
+    var address = 'https://www.googleapis.com/youtube/v3/search';
+    superAgent
+    .get(address)
+    .query({ part: 'snippet',
+            maxResults: 25,
+            q: '' + data.keyword,
+            key: process.env.YOUTUBE_API_KEY
+    })
+    .timeout(1000)
+    .end((err, data) => {
+      if (err) console.log(err);
+      res.status(200).json(data);
+    });
   });
 });
