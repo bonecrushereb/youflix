e4045d31b60e161b20a41c3f623c7b1f88dec062
bonecrushereb <bonecrushereb@gmail.com>
2017-06-27 10:51:11 -0700

    server test passing

---
+++ backend/test/test_setup.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const port = process.env.PORT = 5000;
+const server = require(__dirname + '/../../server');
+process.env.MONGODB_URI = 'mongodb://localhost/youtube_test_db';
+module.exports = exports = (callback) => {
+  mongoose.connect(process.env.MONGODB_URI, () => {
+    server.listen(port, () => {
+      console.log('server up on port: ' + port);
+      callback();
+    })
+  })
+}
