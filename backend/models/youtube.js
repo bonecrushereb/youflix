5c46dd648e9ade5d8f35b93c6c251b65914d3f4e
bonecrushereb <bonecrushereb@gmail.com>
2017-06-28 10:21:47 -0700

    able to post to category test

--- backend/models/youtube.js
+++ backend/models/youtube.js
@@ -5,7 +5,8 @@ var youtubeSchema = new mongoose.Schema({
   title: { type: String },
   description: { type: String },
   videoId: { type: String },
-  channelTitle: { type: String }
+  channelTitle: { type: String },
+  like: { type: String }
 });
 
 module.exports = exports = mongoose.model('youtube', youtubeSchema);
