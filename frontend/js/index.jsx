859ebf4a77a000aebf7c63eb3bdebf974b9a5b7c
bonecrushereb <bonecrushereb@gmail.com>
2017-07-10 10:47:20 -0700

    running with angular updated gulpfile for angular

--- frontend/js/index.jsx
+++
@@ -1,5 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const CategoryList = require('./categories.jsx');
-
-ReactDOM.render(<CategoryList />, document.getElementById('content'));
