57e3c47fc037e5d07ec436264751e8fc6c144170
bonecrushereb <bonecrushereb@gmail.com>
2018-04-09 10:21:56 -0700

    backend working

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const youtubeRouter = require(__dirname + '/backend/routes/youtuberouter');
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGOD_URI || 'mongodb://localhost/youtube_db');
+
+app.use('/api', youtubeRouter);
 
 app.use((req, res) => {
   res.status(404).send('Page not found');
