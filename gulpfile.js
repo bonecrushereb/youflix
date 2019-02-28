81dc2f0d60f68702a685db694da080d9fb7afe1d
bonecrushereb <bonecrushereb@gmail.com>
2019-02-27 23:32:52 -0800

    converted gulpfile to only work with backend setup intial file structure forfront end

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,10 @@
 const gulp = require('gulp');
-
-//  Backend Imports
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 
-let backendFiles = ['./backend/*.js', './backend/lib/**/*.js',
-                    './backend/routes/**/*.js',
-                    './backend/models/**/*.js'
-                  ];
-let testFiles = ['./backend/test/**/*.js'];
+var appFiles = ['*.js', './lib/**/*.js', './backend/routes/**/*.js', './backend/models/**/*.js'];
+var testFiles = ['./backend/test/**/*.js'];
 
-//  FronEnd Imports
-// const uglify = require('gulp-uglify');
-const htmlreplace = require('gulp-html-replace');
-const source = require('vinyl-source-stream');
-const browserify = require('browserify');
-const watchify = require('watchify');
-const reactify = require('reactify');
-// const streamify = require('gulp-streamify');
-
-let frontEndFiles = {
-  HTML: 'frontend/public/index.html',
-  MINIFIED_OUT: 'build.min.js',
-  OUT: 'build.js',
-  DEST: 'dist',
-  DEST_BUILD: 'dist/build',
-  DEST_SRC: 'dist/src',
-  ENTRY_POINT: './frontend/src/app.js'
-};
-
-//  Backend Tasks
 gulp.task('test:mocha', () => {
   return gulp.src(testFiles)
     .pipe(mocha());
@@ -41,61 +16,17 @@ gulp.task('lint:testFiles', () => {
     .pipe(eslint.format());
 });
 
-gulp.task('lint:backendFiles', () => {
-  return gulp.src(backendFiles)
+gulp.task('lint:appFiles', () => {
+  return gulp.src(appFiles)
     .pipe(eslint())
     .pipe(eslint.format());
 });
 
-//  FrontEnd tasks
-gulp.task('copy', () => {
-  gulp.src(frontEndFiles.HTML)
-    .pipe(gulp.dest(frontEndFiles.DEST));
-});
-
-gulp.task('watch', () => {
-  gulp.watch(frontEndFiles.HMTL, ['copy']);
-
-  const watcher = watchify(browserify({
-    entries: [frontEndFiles.ENTRY_POINT],
-    transform: [reactify],
-    debug: true,
-    cache: {}, packageCache: {}, fullPaths: true
-  }));
-
-  return watcher.on('update', () => {
-    watcher.bundle()
-      .pipe(source(frontEndFiles.OUT))
-      .pipe(gulp.dest(frontEndFiles.DEST_SRC));
-      console.log('Updated');
-  })
-    .bundle()
-    .pipe(source(frontEndFiles.OUT))
-    .pipe(gulp.dest(frontEndFiles.DEST_SRC));
-});
-
-gulp.task('build', () => {
-  browserify({
-    entries: [frontEndFiles.ENTRY_POINT],
-    transform: [reactify]
-  })
-    .bundle()
-    .pipe(source(frontEndFiles.MINIFIED_OUT))
-    .pipe(gulp.dest(frontEndFiles.DEST_BUILD));
-});
-
-gulp.task('replaceHTMl', () => {
-  gulp.src(frontEndFiles.HTML)
-    .pipe(htmlreplace({
-      'js': 'build/' + frontEndFiles.MINIFIED_OUT
-    }))
-    .pipe(gulp.dest(frontEndFiles.DEST));
-});
-
-//  Processes
 gulp.task('test', ['test:mocha']);
-gulp.task('lint', ['lint:testFiles', 'lint:backendFiles']);
-gulp.task('production', ['replaceHTMl', 'build']);
-gulp.task('watch', ['watch']);
+gulp.task('lint', ['lint:testFiles', 'lint:appFiles']);
+gulp.task('watch', () => {
+  gulp.watch(testFiles, ['test:mocha', 'lint:testFiles']);
+  gulp.watch(appFiles, ['test:mocha', 'lint:appFiles']);
+});
 
-gulp.task('default', ['lint', 'test', 'watch']);
+gulp.task('default', ['watch', 'lint', 'test']);
