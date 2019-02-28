fd9e2d9d29ea6346a2419f81a041995b737b451b
bonecrushereb <bonecrushereb@gmail.com>
2019-02-28 12:03:13 -0800

    add validator to package.json for later intial setup of frontend

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import YouFlixApp from './components/YouFlixApp';
+import InDecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<YouFlixApp/>, document.getElementById('app'));
+ReactDOM.render(<InDecisionApp/>, document.getElementById('app'));
